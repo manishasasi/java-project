1029a144e22ff95c173208a886e6f84b23a8ae5b
manishasasi <manishasasidharansankar@gmail.com>
2025-11-29 19:18:12 +0800

    Update HelloWorld.java

--- java-project/src/HelloWorld.java
+++ java-project/src/HelloWorld.java
@@ -1,5 +1,19 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloDockerWorld {
-    public static void main(String[] args) {
-        System.out.println("Vanakkam from VS Code!");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello Everyone, My self manisha!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port 8080...");
     }
 }
