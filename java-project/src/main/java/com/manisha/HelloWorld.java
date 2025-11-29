e30fcb3837ba0ac1215f93a5eb05138bcdc65744
manishasasi <manishasasidharansankar@gmail.com>
2025-11-29 23:55:14 +0800

    Update HelloWorld.java

--- java-project/src/main/java/com/manisha/HelloWorld.java
+++ java-project/src/main/java/com/manisha/HelloWorld.java
@@ -2,6 +2,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+package com.manisha;
+
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
