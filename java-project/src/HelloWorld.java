bc42af21eede0714b2905441defcd50f3c8107d8
manishasasi <manishasasidharansankar@gmail.com>
2025-11-29 23:32:45 +0800

    update floder

--- java-project/src/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloDockerWorld {
-    public static void main(String[] args) {
-        System.out.println("Vanakkam from VS Code!");
-    }
-}
---
+++ java-project/src/main/java/com/manisha/HelloWorld.java
@@ -0,0 +1,19 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello Everyone, My self manisha!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port 8080...");
+    }
+}
